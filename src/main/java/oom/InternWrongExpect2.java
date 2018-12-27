8194ffbe7f227d8e0bb8994754fc34abd1253a84
fionchou <fionchou@163.com>
2018-12-27 18:20:12 +0800

    修改

--- src/main/java/oom/InternWrongExpect2.java
+++ src/main/java/oom/InternWrongExpect2.java
@@ -15,8 +15,9 @@ public class InternWrongExpect2 {
         System.out.println(str1.intern() == str1);
 
 
-        //对象的引用  //带拼接操作的，会自动创建常量池的实例对象
+        //对象的引用
         String str10 = new StringBuilder("1x").append("d").toString();
+        //带拼接操作的，没有就创建常量池的实例对象，并返回引用，是堆里的引用和常量池的其实是同一个
         //计算机软件 首次不在常量池中，intern()在记录对象的引用，并返回引用
         System.out.println(str10.intern() == str10);
 
